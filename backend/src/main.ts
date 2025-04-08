a181eebe79c1e3dd76d7d04fdd5500d33bdfe5cd
Evgeny <evgeny.shvetcov@mail.ru>
2025-04-08 23:50:51 +0300

    mvp fix

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,23 +2,30 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger'; // 👈 импорт Swagger
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = app.get(ConfigService);
+
+  app.enableCors({
+    origin: config.get<string>('CORS_ORIGIN'),
+    credentials: true,
+  });
 
   app.useGlobalPipes(new ValidationPipe());
 
   // ✅ Swagger конфигурация
-  const config = new DocumentBuilder()
+  const swaggerConfig = new DocumentBuilder()
     .setTitle('Medival Game API')
     .setDescription('API для управления игроками и юнитами')
     .setVersion('1.0')
     .addBearerAuth()
     .build();
 
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document); // <-- доступно по /api
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(process.env.PORT ?? 3000);
 }
