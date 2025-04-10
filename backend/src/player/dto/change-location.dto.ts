d22485abb4e542c7bde437a734d4c7d7fb3df9fc
Evgeny <evgeny.shvetcov@mail.ru>
2025-04-10 06:41:17 +0300

    fix

--- backend/src/player/dto/change-location.dto.ts
+++ backend/src/player/dto/change-location.dto.ts
@@ -4,5 +4,5 @@ import { ApiProperty } from '@nestjs/swagger';
 export class ChangeLocationDto {
   @ApiProperty({ description: 'ID новой локации', example: 'uuid-location' })
   @IsUUID()
-  locationId: string;
+  locationKey: string;
 }
