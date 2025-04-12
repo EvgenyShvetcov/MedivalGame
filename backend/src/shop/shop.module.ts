670f34490bc91761ae4e7471ed1ec4a63db2f3f2
Evgeny <evgeny.shvetcov@mail.ru>
2025-04-12 10:39:20 +0300

    123

--- backend/src/shop/shop.module.ts
+++ backend/src/shop/shop.module.ts
@@ -1,8 +1,17 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm'; // 💥 Не забудь это!
 import { ShopService } from './shop.service';
 import { ShopController } from './shop.controller';
+import { Shop } from './entities/shop.entity';
+import { ShopItem } from 'src/shop-item/entities/shop-item.entity';
+import { Player } from 'src/player/entities/player.entity';
+import { Item } from 'src/item/entities/item.entity';
+import { PlayerItem } from 'src/item/entities/player-item.entity';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([Shop, ShopItem, Player, Item, PlayerItem]),
+  ],
   controllers: [ShopController],
   providers: [ShopService],
 })
